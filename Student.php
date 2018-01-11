63866c70b26f8bb436266c82ae9c387a39bda291
Alisa <Alisa@192.168.0.4>
2018-01-11 13:45:54 -0800

    Space Format

--- Student.php
+++ Student.php
@@ -13,6 +13,7 @@
  */
 class Student {
     
+    //Add a constructor
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,14 +21,17 @@ class Student {
         $this->grades = array();
     }
     
+    //Add the email address
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    //Add the grades
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Get the average grade
     function average() {
         $total = 0;
         foreach ($this->grades as $value) {
@@ -36,11 +40,16 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //ToString function
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
         foreach ($this->emails as $which => $what) {
-            $result .= $which . ': ' . $what . "\n";
+            if(strlen($which) > 7){
+                $result .= $which . ":\t" . $what . "\n";
+            }else{
+                $result .= $which . ":\t\t" . $what . "\n";
+            }
         }
         $result .= "\n";
         return '<pre>'.$result.'</pre>';
